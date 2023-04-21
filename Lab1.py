2a9202fff7f550c4811f469eab1839c3b199e386
Thierry K <thierry.kamagne@gmail.com>
2023-04-21 17:57:20 +0000

    Add definition

--- Lab1.py
+++ Lab1.py
@@ -6,6 +6,9 @@ iam_client = boto3.client('iam',
         aws_access_key_id='${AWS_ACCESS_KEY_ID}',
         aws_secret_access_key='${AWS_SECRET_ACCESS_KEY}')
 
+# To createe an IAM role ypou will need 
+# 1. Define policy that will be attach to a role
+
 assume_policy = json.dumps({
     "Version": "2012-10-17",
     "Statement": [
@@ -23,12 +26,15 @@ assume_policy = json.dumps({
     ]
 })
 
+# 2. Create role with its name and policy
+
 response = iam_client.create_role(
     RoleName='myRedshiftRole',
     AssumeRolePolicyDocument=assume_policy)
 
 print(response["Role"]["RoleName"])
 
+# 3. Finally attach the policy to the new created role
 resp = iam_client.attach_role_policy(
     RoleName='myRedshiftRole',
         PolicyArn='arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess'
